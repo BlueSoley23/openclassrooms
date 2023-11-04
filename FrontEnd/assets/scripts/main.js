ba5e7323b218ccb65e63562fe141c052a90a7edb
BlueSoley23 <bluegauer@gmail.com>
2023-11-04 23:27:03 +0100

    1104-2324 - Résolution de l'importation de la fonction getAndPrintWorks(), déclarée dans le module gallery.js et du script principal de type module, main.js - Formattage et ajout de commentaires

--- FrontEnd/assets/scripts/main.js
+++ FrontEnd/assets/scripts/main.js
@@ -1,42 +1,19 @@
-/*
-// Importation de la fonction getAndPrintWorks déclarée dans le fichier FrontEnd/assets/scripts/components/gallery.js
-import { getAndPrintWorks } from './components/gallery.js';
+//Affichage des oeuvres sur la page d'accueil
 
-// Recupération et affichage des oeuvres avec l'appel de la fonction getAndPrintWorks
-    getAndPrintWorks();
-*/
+    // Importation de la fonction getAndPrintWorks déclarée dans le fichier FrontEnd/assets/scripts/components/gallery.js
+        import { getAndPrintWorks } from './components/gallery.js';
 
-async function getAndPrintWorks() {
-    try {
-        // Récupération de l'élément du DOM qui contiendra les oeuvres
-        const gallery = document.querySelector('.gallery');
+    // Recupération et affichage des oeuvres avec l'appel de la fonction getAndPrintWorks
+        getAndPrintWorks();
+        
 
-        // Envoie une requête GET à l'API pour récupérer les données des oeuvres
-        const response = await fetch('http://localhost:5678/api/works');
-        const data = await response.json();
+// Création des filtres de catégories
+    /* 
+    Suggestion de l'IA :
 
-        // Parcourt les données et affiche chaque oeuvre dans la console
-        for (let item of data) {
-            // Création d'un élément HTML qui contiendra une oeuvre
-            const workElement = document.createElement('figure');
+    // Importation de la fonction getCategories déclarée dans le fichier FrontEnd/assets/scripts/components/categories.js
+        import { getCategories } from './components/categories.js';
 
-            // Création de chaque élément HTML <figure> correspondant à chaque oeuvre avec son titre et son image
-            workElement.innerHTML = `
-                <img src="${item.imageUrl}" alt="${item.title}">
-                <figcaption>${item.title}</figcaption>
-            `;
-
-            // Ajoute l'élément HTML dans le DOM
-            gallery.appendChild(workElement);
-        }
-    } 
-    
-    // Retourne le message d'erreur potentiel
-    catch (error) {
-        // Affiche le message d'erreur potentiel dans une boîte de dialogue
-        alert('Veuillez contacter votre administrateur système en lui indiquant ce message d\'erreur :\n\n' + error);
-        }
-}
-
-// Appel de la fonction getAndPrintWorks
-getAndPrintWorks();
+    // Recupération et affichage des catégories avec l'appel de la fonction getCategories
+        getCategories();
+    */
