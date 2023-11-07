7a880fa43901ddde372b2ab2dc2001f4bfae7bf1
bluesoley23 <bluegauer@gmail.com>
2023-11-07 20:55:06 +0100

    1107-2054 - Finalisation de l'affichage dynamique des projets.

--- FrontEnd/assets/scripts/components/gallery.js
+++ FrontEnd/assets/scripts/components/gallery.js
@@ -1,36 +1,71 @@
-// Récupération et affichage des oeuvres
-    export async function getAndPrintWorks() {
-        try {
+// ##################################################################################
+// ##############################   getWorks()   ############################### 
+// #################   Récupération des travaux depuis l'API   #################
+// ##################################################################################
 
-            // Récupération de l'élément du DOM qui contiendra les oeuvres
-                const gallery = document.querySelector('.gallery');
+    // Déclaration de la fonction asynchrone qui récupère les travaux depuis l'API
+    export async function getWorks() {
+        
+        // Envoie une requête GET à l'API pour récupérer les données des travaux, et attend la réponse avant de continuer l'exécution du code grace à await
+        return await fetch('http://localhost:5678/api/works')
 
-            // Envoie une requête GET à l'API pour récupérer les données des oeuvres
-                const response = await fetch('http://localhost:5678/api/works');
-                const data = await response.json();
+            // Si la réponse est reçue, on la convertit en JSON et on la retourne
+            .then(response => response.json())
 
-            // Parcourt les données et affiche chaque oeuvre dans la console
-                for (let item of data) {
-
-                    // Création d'un élément HTML qui contiendra une oeuvre
-                        const workElement = document.createElement('figure');
-
-                    // Création de chaque élément HTML <figure> correspondant à chaque oeuvre avec son titre et son image
-                        workElement.innerHTML = `
-                            <img src="${item.imageUrl}" alt="${item.title}">
-                            <figcaption>${item.title}</figcaption>
-                        `;
-
-                    // Ajoute l'élément HTML dans le DOM
-                        gallery.appendChild(workElement);
-            }
-        } 
-    
-    // Retourne le message d'erreur potentiel
-        catch (error) {
-            
-            // Affiche le message d'erreur potentiel dans une boîte de dialogue
+            // Si la réponse n'est pas reçue, on affiche une boîte de dialogue avec le message d'erreur
+            .catch(error => {
                 alert('Veuillez contacter votre administrateur système en lui indiquant ce message d\'erreur :\n\n' + error);
-            }
+            });
     }
 
+
+
+// ##################################################################################
+// ##############################   printWorks()   ############################### 
+// ###############   Affichage des travaux sur la page d'accueil   ###############
+// ##################################################################################
+
+    // Déclaration de la fonction asynchrone qui affiche les travaux sur la page d'accueil
+    export async function printWorks(works) {
+
+        // Récupération de l'élément du DOM qui contiendra les travaux
+        const gallery = document.querySelector('.gallery');
+
+        // Parcourt les données et affiche chaque travail dans la console
+        for (let item of works) {
+
+            // Création d'un élément HTML qui contiendra un travail
+            const workElement = document.createElement('figure');
+
+            // Création de chaque élément HTML <figure> correspondant à chaque travail avec son titre et son image
+            workElement.innerHTML = `
+                <img src="${item.imageUrl}" alt="${item.title}">
+                <figcaption>${item.title}</figcaption>
+            `;
+
+            // Rattache l'élément HTML dans le DOM (le projet est affiché dansla section .gallery)
+            gallery.appendChild(workElement);
+        }
+    
+    }
+
+
+
+// ##################################################################################
+// ##############################   getCategories()   ############################### 
+// ##########   Affichage de boutons filtrant les travaux par catégories   ##########
+// ##################################################################################
+
+    // Déclaration de la fonction asynchrone qui récupère les catégories depuis l'API
+    export async function getCategories() {
+
+        // Appel de la fonction getWorks() pour récupérer les données des travaux
+        const works = await getWorks();
+
+        // Création d'un objet set qui permet de filtrer les doublons
+        const categories = new Set();
+
+        // Parcourt les données des travaux et récupère les catégories de chaque projet pour les ajouter dans l'objet set
+        
+
+    }
--- FrontEnd/assets/scripts/main.js
+++ FrontEnd/assets/scripts/main.js
@@ -1,19 +1,16 @@
+// Documentation Swagger : http://localhost:5678/api-docs/
+// Page Front-end : http://127.0.0.1:5500/FrontEnd/
+
 //Affichage des oeuvres sur la page d'accueil
 
-    // Importation de la fonction getAndPrintWorks déclarée dans le fichier FrontEnd/assets/scripts/components/gallery.js
-        import { getAndPrintWorks } from './components/gallery.js';
+    // Importation de la fonction getWorks déclarée dans le fichier FrontEnd/assets/scripts/components/gallery.js
+        import { getWorks } from './components/gallery.js';
 
-    // Recupération et affichage des oeuvres avec l'appel de la fonction getAndPrintWorks
-        getAndPrintWorks();
+    // Importation de la fonction printWorks déclarée dans le fichier FrontEnd/assets/scripts/components/gallery.js
+        import { printWorks } from './components/gallery.js';
         
 
-// Création des filtres de catégories
-    /* 
-    Suggestion de l'IA :
 
-    // Importation de la fonction getCategories déclarée dans le fichier FrontEnd/assets/scripts/components/categories.js
-        import { getCategories } from './components/categories.js';
-
-    // Recupération et affichage des catégories avec l'appel de la fonction getCategories
-        getCategories();
-    */
+    // Recupération et affichage des oeuvres avec l'appel des fonctions getWorks et printWorks
+        getWorks().then(works => printWorks(works));
+        
