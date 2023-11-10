e116d84a831355a00cb9183ce27e7410b15575ce
bluesoley23 <bluegauer@gmail.com>
2023-11-10 19:18:00 +0100

    1110-1918 - Ajout des boutons filtres de catégories et formattage des fichiers JS, fonctionnalité de tri à intégrer...

--- FrontEnd/assets/scripts/components/gallery.js
+++ FrontEnd/assets/scripts/components/gallery.js
@@ -1,125 +1,109 @@
-// ###################################################################################
-// #################################   getWorks()   ##################################
-// ####################   Récupération des travaux depuis l'API   ####################
-// ###################################################################################
+// #######################################################################################################
+// ###########################################   getWorks()   ############################################
+// ##############################   Récupération des travaux depuis l'API   ##############################
+// #######################################################################################################
 
     // Déclaration de la fonction asynchrone qui récupère les travaux depuis l'API
-        export async function getWorks() {
-        
-        // Envoie une requête GET à l'API pour récupérer les données des travaux, et attend la réponse avant de continuer l'exécution du code grace à await
-            return await fetch('http://localhost:5678/api/works')
+    export async function getWorks() {
+
+        // Envoie une requête GET à l'API pour récupérer les données des travaux, et attend la réponse avant
+        // de continuer l'exécution du code grace à await 
+        return await fetch('http://localhost:5678/api/works')
 
             // Si la réponse est reçue, on la convertit en JSON et on la retourne
-                .then(response => response.json())
+            .then(response => response.json())
 
             // Si la réponse n'est pas reçue, on affiche une boîte de dialogue avec le message d'erreur
-                .catch(error => {
-                    alert('Veuillez contacter votre administrateur système en lui indiquant ce message d\'erreur :\n\n' + error);
+            .catch(error => {
+                alert('Veuillez contacter votre administrateur système en lui indiquant ce message d\'erreur :\n\n' + error);
+            });
 
-                });
-
-        }
-
+    }
 
 
-// ###################################################################################
-// ###############################   printWorks()   ##################################
-// ################   Affichage des travaux sur la page d'accueil   ##################
-// ###################################################################################
+
+// #######################################################################################################
+// #########################################   printWorks()   ############################################
+// ##########################   Affichage des travaux sur la page d'accueil   ############################
+// #######################################################################################################
 
     // Déclaration de la fonction asynchrone qui affiche les travaux sur la page d'accueil
-        export default function printWorks(works) {
+    export default function printWorks(works) {
 
-            try {
+        try {
 
-                // Récupération de l'élément du DOM qui contiendra les travaux
-                    const gallery = document.querySelector('.gallery');
+            // Récupération de l'élément du DOM qui contiendra les travaux
+            const gallery = document.querySelector('.gallery');
 
-                // Parcourt les données et affiche chaque travail dans la console
-                    for (let item of works) {
+            // Parcourt les données et affiche chaque travail dans la console
+            for (let item of works) {
 
-                    // Création d'un élément HTML qui contiendra un travail
-                        const workElement = document.createElement('figure');
+                // Création d'un élément HTML qui contiendra un travail
+                const workElement = document.createElement('figure');
 
-                    // Création de chaque élément HTML <figure> correspondant à chaque travail avec son titre et son image
-                        workElement.innerHTML = `
-                            <img src="${item.imageUrl}" alt="${item.title}">
-                            <figcaption>${item.title}</figcaption>
-                        `;
+                // Création de chaque élément HTML <figure> correspondant à chaque travail avec son titre
+                // et son image 
+                workElement.innerHTML = `
+                    <img src="${item.imageUrl}" alt="${item.title}">
+                    <figcaption>${item.title}</figcaption>
+                `;
 
-                    // Rattache l'élément HTML dans le DOM (le projet est affiché dansla section .gallery)
-                        gallery.appendChild(workElement);
+                // Rattache l'élément HTML dans le DOM (le projet est affiché dansla section .gallery)
+                gallery.appendChild(workElement);
 
-                    }
-                    
             }
 
-                
-
-            catch (error)
-
-                {
-                    alert('Veuillez contacter votre administrateur système en lui indiquant ce message d\'erreur :\n\n' + error);
-                }    
+        } catch (error) {
+            alert('Veuillez contacter votre administrateur système en lui indiquant ce message d\'erreur :\n\n' + error);
         }
+    }
 
 
 
-// ###################################################################################
-// ##############################   getCategories()   ################################
-// ################   Renvoie la liste des catégories sans doublons   ################
-// ###################################################################################
+// #######################################################################################################
+// ########################################   getCategories()   ##########################################
+// ##########################   Renvoie la liste des catégories sans doublons   ##########################
+// #######################################################################################################
 
-    // Déclaration de la fonction asynchrone qui récupère les catégories depuis l'API
-        export async function getCategories() {
+    // Déclaration de la fonction asynchrone qui récupère les catégories des travaux sans doublons
+    export async function getCategories() {
 
-        // Appel de la fonction getWorks() pour récupérer les données des travaux sous forme de tableau d'objets JSON
-            let works = await getWorks();
+        // Récupération des travaux depuis l'API
+        let works = await getWorks();
 
-        // Extraction des catégories des travaux dans un objet Set afin d'éviter les doublons
-            const categories = new Set(works.map(work => work.category));
+        // Création d'un Set contenant les noms des catégories sans doublons
+        const categories = new Set(works.map(work => work.category.name));
 
-        // Conversion de l'objet Set en tableau afin de pouvoir utiliser les méthodes de tableau
-            return Array.from(categories);
-            
-        }
-
+        // Conversion du Set en Array et retourne le résultat
+        return Array.from(categories);
+    }
 
 
-// ###################################################################################        
-// ################################ printCategories() ################################        
-// ############ Affiche des boutons filtrant les travaux par catégories ##############        
-// ###################################################################################  
 
+// #######################################################################################################        
+// ########################################   printCategories()   ########################################      
+// ####################   Affiche des boutons filtrant les travaux par catégories   ######################        
+// #######################################################################################################  
 
-    // Déclaration de la fonction asynchrone qui affiche les boutons de filtrage par catégories
-        export async function printCategories() {
+    // Déclaration de la fonction asynchrone qui affiche les boutons de filtrage par catégorie
+    export async function printCategories() {
 
-        // Appel de la fonction getCategories() pour récupérer les catégories des travaux
-            const categories = await getCategories();
-           
+        // Récupération des catégories depuis l'API
+        const categories = await getCategories();
+
         // Récupération de l'élément du DOM qui contiendra les boutons de filtrage
-            const categoriesSection = document.querySelector('.categories');
-            
+        const categoriesSection = document.querySelector('.categories');
 
+        // Parcourt les catégories et crée un bouton pour chaque catégorie
+        for (let category of categories) {
 
+            // Création d'un élément HTML qui contiendra un bouton de filtrage
+            const buttonElement = document.createElement('button');
 
-        // Parcourt les catégories et les ajoute dans des boutons dans le DOM
-            for (let category of categories) {
-                console.log('category :' + category);
-                console.log('categories :' + categories);
-
-            // Création d'un élément HTML qui contiendra un bouton
-                const buttonElement = document.createElement('button');
-                console.log('buttonElement :' + buttonElement);
-
-                buttonElement.innerHTML = `${category}`;
-                console.log(category + 'et' + categories);
+            // Ajout du nom de la catégorie dans le texte du bouton
+            buttonElement.innerHTML = `${category}`;
 
             // Rattache l'élément HTML dans le DOM (le bouton est affiché dans la section .categories)
-                categoriesSection.appendChild(buttonElement);
-                console.log('categoriesSection :' + categoriesSection);
-                console.log('buttonElement :' + buttonElement);
-
-            }
+            categoriesSection.appendChild(buttonElement);
         }
+    }
--- FrontEnd/assets/scripts/main.js
+++ FrontEnd/assets/scripts/main.js
@@ -1,29 +1,37 @@
-// Documentation Swagger : http://localhost:5678/api-docs/
-// Page Front-end : http://127.0.0.1:5500/FrontEnd/
+// #######################################################################################################
+// ###################   Documentation Swagger : http://localhost:5678/api-docs/   #######################
+// ######################   Page Front-end : http://127.0.0.1:5500/FrontEnd/   ###########################
+// #######################################################################################################
 
-//Affichage des oeuvres sur la page d'accueil
+
+
+// #######################################################################################################
+// ############################   Affichage des oeuvres sur la page d'accueil   ##########################
+// #######################################################################################################
 
     // Importation de la fonction getWorks déclarée dans le fichier FrontEnd/assets/scripts/components/gallery.js
-        import { getWorks } from './components/gallery.js';
+    import { getWorks } from './components/gallery.js';
 
     // Importation de la fonction printWorks déclarée dans le fichier FrontEnd/assets/scripts/components/gallery.js
-        import printWorks from './components/gallery.js';
+    import printWorks from './components/gallery.js';
         
 
     // Recupération et affichage des oeuvres avec l'appel des fonctions getWorks et printWorks
-        getWorks().then(works => printWorks(works));
+    getWorks().then(works => printWorks(works));
         
 
 
-//Affichage des boutons de catégories sur la page d'accueil
 
-    // Importation de la fonction getCategories déclarée dans le fichier FrontEnd/assets/scripts/components/gallery.js
-        import { getCategories } from './components/gallery.js';
+// #######################################################################################################
+// ####################   Affichage des boutons de catégories sur la page d'accueil   ####################
+// #######################################################################################################
 
-    // Importation de la fonction getCategories déclarée dans le fichier FrontEnd/assets/scripts/components/gallery.js
-        import { printCategories } from './components/gallery.js';
+    // Importation de la fonction getCategories déclarée dans le fichiergallery.js
+    import { getCategories } from './components/gallery.js';
+
+    // Importation de la fonction getCategories déclarée dans le fichier gallery.js
+    import { printCategories } from './components/gallery.js';
 
 
     // Recupération et affichage des catégories avec l'appel de la fonction getCategories
-        getCategories().then(categories => printCategories(categories));
-    
+    getCategories().then(categories => printCategories(categories));
