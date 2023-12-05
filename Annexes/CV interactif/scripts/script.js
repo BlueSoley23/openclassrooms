f5e5a14591532fe4185ab019bc9530f14ba64d19
BlueSoley22 <bluegauer@gmail.com>
2023-12-05 01:15:56 +0100

    1205-0115 - Commit initial : initialisation du dépôt avec les nfichiers du projet 1

---
+++ Annexes/CV interactif/scripts/script.js
@@ -0,0 +1,63 @@
+//Partie 4 : Lettres de motivation.
+
+    //Appels de la fonction lettreDeMotivation() qui permet d'afficher
+    //une lettre de motivation spécifique au click d'un des boutons des
+    //secteur d'activités. Elle permet de proposer plusieurs lettres.
+    //on la charge avec l'identifiant du bouton souhaité et la lettre de 
+    //motivation au format HTML et dans une chaine de caractères.
+
+    lettreDeMotivation("#informatique", "<i>Ceci est une lettre factice, qui correspond à ce à quoi devrait ressembler ma lettre de motivation, à l'issue de ma formation.</i><br><br><br><br>Madame, Monsieur, <br><br><br><br>Je suis Antoine Gauer, un étudiant passionné de développement informatique depuis mon adolescence. J'ai obtenu un Bac STI2D spécialisé en informatique. Par la présente, je vous soumets ma candidature pour un emploi chez vous; en effet, je recherche un poste qui me permettrait de faire de ma passion la plus profonde mon activité professionnelle. Ainsi, travailler chez vous serait pour moi l'opportunité d'exercer une activité professionnelle adéquate et correspondant à mes compétences.<br><br> En effet, j’ai déjà expérimenté l’informatique; le coté du développeur. Et ces expériences m’ont permis de réaliser que ce serait un emploi qui me plairait beaucoup et qui correspondrait aussi à mes qualités. Je pense avoir les qualifications nécessaires à ce poste; j'aime le travail d'équipe, je suis efficace, organisé et polyvalent. C'est pour cela que travailler dans votre enseigne me tient à cœur. <br><br> Je suis un vrai passionné et je me suis formé au développement web full stack chez OpenClassrooms.<br><br>La formulation d’un algorithme et d’un cahier des charges, le développement d’un prototype, le développement de projets web tels que des sites ou des applications web, le codage et le débogage sont toutes des tâches que je maîtrise très bien. <br>Avec bien sûr, la maîtrise totale des langages HTML/CSS et JavaScript, langages incontournables dans le monde d’aujourd’hui. <br><br> Je suis sûr qu’avec mes compétences je pourrais correctement m’intégrer et compléter votre équipe. <br><br> Ce serait un honneur pour moi de travailler au sein de votre entreprise<br><br> <br><br> Je vous remercie pour votre temps et votre considération. <br><br> Je souhaite que ma candidature ait attiré votre attention, et me permettra de vous rencontrer afin de vous démontrer ma motivation. <br><br> <br><br> Je vous prie d'agréer, Madame, Monsieur, mes respectueuses salutations<br><br> <br><br> Bien cordialement, <br><br> Antoine GAUER.");
+
+    lettreDeMotivation("#restauration","Madame, Monsieur,<br><br><br><br>Je suis Antoine Gauer, un étudiant passionné de développement informatique depuis mon adolescence. J'ai obtenu un Bac STI2D spécialisé en informatique. Par la présente, je vous soumets ma candidature pour un emploi étudiant chez vous; en effet, je recherche un poste à temps partiel de préférence, qui me permettrait de financer mes études. Ainsi, travailler chez vous serait pour moi l'opportunité d'exercer une activité professionnelle en parallèle de mes études, correspondant à mes compétences. <br><br>En effet, j’ai déjà travaillé en restauration et ces expériences m’ont permis de réaliser que c’est un emploi qui me plait et qui correspond à mes qualités. Je pense avoir les qualifications nécessaires à ce poste; j'aime le travail d'équipe, je suis efficace, organisé, polyvalent et à l'aise avec le contact à la clientèle. C'est pour cela que travailler dans votre restaurant me tient à cœur. <br><br>Je suis un employé dynamique, polyvalent et efficace. En plonge comme en cuisine ou en salle, la préparation d’ingrédients, la plonge, et l’encaissement entre autres, sont toutes des tâches que je maîtrise très bien. <br><br>Je suis sûr qu’avec mes compétences je pourrais correctement m’intégrer et compléter votre équipe. <br><br>Ce serait un honneur pour moi de travailler au sein de votre enseigne.<br><br>Au-delà de cela, je suis très flexible sur le plan des heures de travail car ma formation se déroule en ligne. Je n’ai donc aucune contrainte horaire et je suis toujours très ponctuel.<br><br>Je vous remercie pour votre temps et votre considération. <br><br>Je souhaite que ma candidature ait attiré votre attention, et me permettra de vous rencontrer afin de vous démontrer ma motivation. <br><br><br><br>Je vous prie d'agréer, Madame, Monsieur, mes respectueuses salutations.<br><br>Bien cordialement,<br><br>Antoine GAUER.");
+
+    lettreDeMotivation("#grandesSurfaces", "Madame, Monsieur,<br><br><br><br>Je suis Antoine Gauer, un étudiant passionné de développement informatique depuis mon adolescence. J'ai obtenu un Bac STI2D spécialisé en informatique. Par la présente, je vous soumets ma candidature pour un emploi étudiant chez vous; en effet, je recherche un poste à temps partiel de préférence, qui me permettrait de financer mes études.<br><br> Ainsi, travailler chez vous serait pour moi l'opportunité d'exercer une activité professionnelle en parallèle de mes études, correspondant à mes compétences. <br>En effet, j’ai déjà travaillé en grandes surfaces et ces expériences m’ont permis de réaliser que c’est un emploi qui me plait et qui correspond à mes qualités; <br>Je pense avoir les qualifications nécessaires à ce poste; j'aime le travail d'équipe, je suis efficace, organisé, polyvalent et à l'aise avec le contact à la clientèle. C'est pour cela que travailler dans votre magasin me tient à cœur.<br><br>Je suis un ELS (employé libre service) dynamique, polyvalent et efficace. Dans les rayons comme en réserve. Le facing (rangement des rayons, remise des produits à l’avant), la gestion des promotions et des têtes de gondole, la manœuvre des palettes et la gestion des stocks sont toutes des tâches que je maîtrise très bien.<br> Je suis sûr qu’avec mes compétences je pourrais correctement m’intégrer et compléter votre équipe. Ce serait un honneur pour moi de travailler au sein de votre enseigne.<br><br> Au-delà de cela, je suis très flexible sur le plan des heures de travail car ma formation se déroule en ligne.<br> Je n’ai donc aucune contrainte horaire et je suis toujours très ponctuel.<br><br><br> Je vous remercie pour votre temps et votre considération. <br>Je souhaite que ma candidature ait attiré votre attention, et me permettra de vous rencontrer afin de vous démontrer ma motivation.<br><br>Je vous prie d'agréer, Madame, Monsieur, mes respectueuses salutations.<br><br><br> Bien cordialement,<br><br>Antoine GAUER.");
+
+    lettreDeMotivation("#livraison", "Madame, Monsieur, <br><br><br><br>Je suis Antoine Gauer, un étudiant passionné de développement informatique depuis mon adolescence. J'ai obtenu un Bac STI2D spécialisé en informatique. Par la présente, je vous soumets ma candidature pour un emploi étudiant chez vous; en effet, je recherche un poste à temps partiel de préférence, qui me permettrait de financer mes études. Ainsi, travailler chez vous serait pour moi l'opportunité d'exercer une activité professionnelle en parallèle de mes études, correspondant à mes compétences.<br><br> En effet, j’ai déjà travaillé en restauration et ces expériences m’ont permis de réaliser que c’est un emploi qui me plait et qui correspond à mes qualités. Je pense avoir les qualifications nécessaires à ce poste; j'aime le travail d'équipe, je suis efficace, organisé, polyvalent et à l'aise avec le contact à la clientèle. C'est pour cela que travailler dans votre magasin me tient à cœur. <br><br> Je suis un livreur dynamique, polyvalent et efficace. En livraison comme en restaurant, la préparation d’ingrédients, la plonge, et l’encaissement entre autres, sont toutes des tâches que je maîtrise très bien. <br><br> Je suis sûr qu’avec mes compétences je pourrais correctement m’intégrer et compléter votre équipe. <br><br> Ce serait un honneur pour moi de travailler au sein d’une enseigne aussi réputée que la vôtre, de par les prix que vous proposez à votre clientèle et la qualité de vos produits notamment. <br><br> Au-delà de cela, je suis très flexible sur le plan des heures de travail car ma formation se déroule en ligne. Je n’ai donc aucune contrainte horaire et je suis toujours très ponctuel. <br><br> <br><br> Je vous remercie pour votre temps et votre considération. <br><br> Je souhaite que ma candidature ait attiré votre attention, et me permettra de vous rencontrer afin de vous démontrer ma motivation. <br><br> <br><br> Je vous prie d'agréer, Madame, Monsieur, mes respectueuses salutations<br><br> <br><br> Bien cordialement, <br><br> Antoine GAUER.");
+
+    lettreDeMotivation("#interims", "Madame, Monsieur, <br><br><br><br>Je suis Antoine Gauer, un étudiant passionné de développement informatique depuis mon adolescence. J'ai obtenu un Bac STI2D spécialisé en informatique. Par la présente, je vous soumets ma candidature pour un emploi en tant partiel, chez vous; en effet, je recherche des missions qui me permettrait d’assurer mes besoins en tant qu’étudiant. Ainsi, travailler chez vous serait pour moi l'opportunité d'exercer une activité professionnelle adéquate et correspondant à mes compétences.<br><br>En effet, j’ai déjà effectué des missions pour une agence d’intérim et ces expériences m’ont permis de réaliser que c’est un mode de travail qui me plait beaucoup et qui correspondrait aussi à mes qualités. Je pense avoir les qualifications nécessaires à ce poste; j'aime le travail d'équipe mais surtout je suis efficace, organisé et polyvalent. <br><br> Le travail en missions m’intérèsse vraiment pour sa divesité et la flexibilité  apportée. En tant qu’étudiant en distanciel, intérimaire est le métier parfait !<br><br> Je suis sûr qu’avec mes compétences je pourrais correctement m’intégrer et compléter votre équipe. <br><br> Ce serait un honneur pour moi de travailler au sein de votre entreprise<br><br> <br><br> Je vous remercie pour votre temps et votre considération. <br><br> Je souhaite que ma candidature ait attiré votre attention, et me permettra de vous rencontrer afin de vous démontrer ma motivation. <br><br> <br><br> Je vous prie d'agréer, Madame, Monsieur, mes respectueuses salutations<br><br> <br><br> Bien cordialement, <br><br> Antoine GAUER.");
+
+
+
+
+//Partie 5 : espace messagerie et satisfaction.
+ 
+    //Initialisation de EmailJS et identification avec ID utilisateur
+
+         (function(){
+            emailjs.init("f-P1r10-8pwczQWzO"); // Remplacez par votre ID utilisateur EmailJS
+        })();
+    
+ 
+
+    //Envoie du formulaire de messagerie par mail
+
+         document.getElementById('messagerie').addEventListener('submit', function(event) {
+            event.preventDefault();
+
+            // génère le service ID et le template ID de EmailJS
+            emailjs.sendForm('service_fqx44ai', 'template_utok7zg', this)
+                .then(function() {
+                    console.log('SUCCESS!');
+                }, function(error) {
+                    console.log('FAILED...', error);
+                });
+        });
+
+
+
+    //Envoie du formulaire de satisfaction par mail
+
+        document.getElementById('satisfaction').addEventListener('submit', function(event) {
+            event.preventDefault();
+
+            emailjs.sendForm('service_fqx44ai', 'template_dqw3gjo', this)
+                .then(function() {
+                    console.log('SUCCESS!');
+                }, function(error) {
+                    console.log('FAILED...', error);
+                });
+        });
+
+
+
+   
